9e694fbf9cc6d1e1a8c01d223ac9f84c2c75d53f
Josh Duff <me@JoshDuff.com>
2023-01-30 09:40:52 -0600

    Call the function

--- entry.mjs
+++ entry.mjs
@@ -2,10 +2,13 @@ import should_be_a_function from './imported'
 
 const actual_type = typeof should_be_a_function
 
-if (actual_type !== `function`) {
+if (actual_type === `function`) {
+	should_be_a_function()
+} else {
 	console.log(`The default import of "imported" is not a function, even though it's supposed to be`)
 }
 
 if (`default` in should_be_a_function) {
 	console.log(`The default import has a \`default\` property`)
+	should_be_a_function.default()
 }
